cf12ba6d1560d739b3b1a4fe7e70b06cb18a4f81
Emina Salihović <emina.salihovic@edu.fit.ba>
2025-12-06 03:54:49 +0100

    Backend Parts

---
+++ Bella/Bella.WebAPI/Program.cs
@@ -0,0 +1,123 @@
+using Bella.Services.Database;
+using Mapster;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.OpenApi.Models;
+using Bella.WebAPI.Filters;
+using Bella.Services.Services;
+using Bella.Services.Interfaces;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<IRoleService, RoleService>();
+builder.Services.AddTransient<IGenderService, GenderService>();
+builder.Services.AddTransient<ICityService, CityService>();
+builder.Services.AddTransient<IProductService, ProductService>();
+builder.Services.AddTransient<ICategoryService, CategoryService>();
+builder.Services.AddTransient<IManufacturerService, ManufacturerService>();
+builder.Services.AddTransient<ILengthService, LengthService>();
+builder.Services.AddTransient<IHairstyleService, HairstyleService>();
+builder.Services.AddTransient<IFacialHairService, FacialHairService>();
+builder.Services.AddTransient<IDyingService, DyingService>();
+builder.Services.AddTransient<IStatusService, StatusService>();
+builder.Services.AddTransient<IAppointmentService, AppointmentService>();
+builder.Services.AddTransient<IReviewService, ReviewService>();
+builder.Services.AddTransient<IOrderService, OrderService>();
+builder.Services.AddTransient<IOrderItemService, OrderItemService>();
+builder.Services.AddTransient<IAnalyticsService, AnalyticsService>();
+builder.Services.AddTransient<ICartService, CartService>();
+builder.Services.AddTransient<ICartItemService, CartItemService>();
+
+
+// Configure database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=.;Database=BellaDb;User Id=sa;Password=QWEasd123!;TrustServerCertificate=True;Trusted_Connection=True;";
+builder.Services.AddDatabaseServices(connectionString);
+
+// Add configuration
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+builder.Services.AddMapster();
+
+builder.Services.AddAuthentication("BasicAuthentication")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+
+builder.Services.AddControllers(x =>
+    {
+        x.Filters.Add<ExceptionFilter>();
+    }
+);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+// Za dodavanje opisnog teksta pored swagger call-a
+var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+    c.AddSecurityDefinition("BasicAuthentication", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "basic",
+        In = ParameterLocation.Header,
+        Description = "Basic Authorization header using the Bearer scheme."
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "BasicAuthentication" } },
+            new string[] { }
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<BellaDbContext>();
+
+
+    var pendingMigrations = dataContext.Database.GetPendingMigrations().Any();
+
+    if (pendingMigrations)
+    {
+
+        dataContext.Database.Migrate();
+
+
+    }
+    // Train the product recommender model in background after startup
+    _ = Task.Run(async () =>  // The underscore tells the compiler we're intentionally ignoring the result
+    {
+        // Wait a bit for the app to fully start
+        await Task.Delay(2000);
+        using (var trainingScope = app.Services.CreateScope())
+        {
+            Bella.Services.Services.ProductService.TrainRecommenderAtStartup(trainingScope.ServiceProvider);
+        }
+    });
+}
+
+app.Run();
